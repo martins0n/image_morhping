d11fe6b204b19b37507648826d0da1764ce088a0
martins0n <martinmail@mail.ru>
2019-11-04 18:28:30 +0300

    warpy degree parametr

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
 
-
-
-
 fn triang(x: u32, y: u32) -> bool {
     if (x > 200) & (x < 400) & (y > 300) & (y < 600) {
         true
@@ -92,10 +89,28 @@ struct LinePair {
 }
 
 impl LinePair {
-    fn swap_lines(&self) -> Self{
-        LinePair{
+    fn swap_lines(&self) -> Self {
+        LinePair {
             t_line: self.s_line,
-            s_line: self.t_line
+            s_line: self.t_line,
+        }
+    }
+
+    fn turn_to_degree(&self, degree: f32) -> Self {
+        if degree == 1.0 {
+            LinePair {
+                s_line: self.s_line,
+                t_line: self.t_line,
+            }
+        }else{
+            let mut new_t_line = self.s_line;
+            new_t_line.p = self.s_line.p + (self.s_line.p - self.t_line.p).mul(degree);
+            new_t_line.q = self.s_line.q + (self.s_line.q - self.t_line.q).mul(degree);
+            LinePair {
+                s_line: self.s_line,
+                t_line: new_t_line,
+            }
+
         }
     }
 }
@@ -108,13 +123,14 @@ fn distatance(p1: &Vector, p2: &Vector, X: &Vector) -> f32 {
 
 fn warpy<'a>(
     line_pairs: &'a Vec<LinePair>,
-    source: &'a  image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
-    target: &'a  image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    source: &'a image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    target: &'a image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    degree: f32
 ) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
-    
     let (imgx, imgy) = target.dimensions();
     let mut warpy_target: image::ImageBuffer<image::Rgb<u8>, _> =
         image::ImageBuffer::new(imgx, imgy);
+    let line_pairs_mod: Vec<LinePair> = line_pairs.iter().map(|x| x.turn_to_degree(degree)).collect();
     for (x, y, pixel) in warpy_target.enumerate_pixels_mut() {
         let mut dsum = Vector { x: 0.0, y: 0.0 };
         let mut weightsum = 0.0;
@@ -122,7 +138,7 @@ fn warpy<'a>(
             x: x as f32,
             y: y as f32,
         };
-        for lp in line_pairs.iter() {
+        for lp in line_pairs_mod.iter() {
             let u =
                 (X - lp.t_line.p) * (lp.t_line.q - lp.t_line.p) / lp.t_line.q.dist2(lp.t_line.p);
             let v = ((X - lp.t_line.p) * (lp.t_line.q - lp.t_line.p).perpendicular())
@@ -144,7 +160,11 @@ fn warpy<'a>(
         }
         let X_new = X + dsum.mul(1.0 / weightsum);
 
-        if (X_new.x <= (imgx - 1) as f32) & (X_new.x >= 0.0) & (X_new.y <= (imgy - 1) as f32) & (X_new.y >= 0.0) {
+        if (X_new.x <= (imgx - 1) as f32)
+            & (X_new.x >= 0.0)
+            & (X_new.y <= (imgy - 1) as f32)
+            & (X_new.y >= 0.0)
+        {
             let t_pixel = source.get_pixel(X_new.x.floor() as u32, X_new.y.floor() as u32);
             *pixel = t_pixel.clone();
         } else {
@@ -156,26 +176,32 @@ fn warpy<'a>(
 }
 
 fn cross_dissolve<'a>(
-    source_image:  &'a  image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
-    image_after_warm: &'a  image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
-    delta: f32
+    source_image: &'a image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    image_after_warm: &'a image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    delta: f32,
 ) -> Vec<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>> {
     let iterations = (1.0 / delta) as i32;
     let mut cd_images = Vec::<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>>::new();
     let (imgx, imgy) = source_image.dimensions();
     for i in 0..iterations {
-    
-        let mut temp_image: image::ImageBuffer<image::Rgb<u8>, _> = image::ImageBuffer::new(imgx, imgy);
+        let mut temp_image: image::ImageBuffer<image::Rgb<u8>, _> =
+            image::ImageBuffer::new(imgx, imgy);
         for (x, y, pixel) in temp_image.enumerate_pixels_mut() {
-            let si_pixel = source_image.get_pixel(x,y);
-            let iaw_pixel = image_after_warm.get_pixel(x,y);
-            let r = (si_pixel[0] as f32 + i as f32 * delta * (iaw_pixel[0] as f32 - si_pixel[0] as f32)) as u8;
-            let g = (si_pixel[1] as f32 + i as f32 * delta * (iaw_pixel[1] as f32 - si_pixel[1] as f32)) as u8;
-            let b = (si_pixel[2] as f32 + i as f32 * delta * (iaw_pixel[2] as f32 - si_pixel[2] as f32)) as u8;
+            let si_pixel = source_image.get_pixel(x, y);
+            let iaw_pixel = image_after_warm.get_pixel(x, y);
+            let r = (si_pixel[0] as f32
+                + i as f32 * delta * (iaw_pixel[0] as f32 - si_pixel[0] as f32))
+                as u8;
+            let g = (si_pixel[1] as f32
+                + i as f32 * delta * (iaw_pixel[1] as f32 - si_pixel[1] as f32))
+                as u8;
+            let b = (si_pixel[2] as f32
+                + i as f32 * delta * (iaw_pixel[2] as f32 - si_pixel[2] as f32))
+                as u8;
             *pixel = image::Rgb([r, g, b]);
         }
         cd_images.push(temp_image);
-    }   
+    }
     cd_images
 }
 
@@ -205,7 +231,7 @@ fn test_with_simple_polygons() {
             *pixel = image::Rgb([r, 0, b]);
         } else {
             *pixel = image::Rgb([0, 0, 0]);
-        } 
+        }
     }
 
     imgbufsource.save("./data/source.png").unwrap();
@@ -234,21 +260,19 @@ fn test_with_simple_polygons() {
         },
     ];
     let inv_lp = lp.iter().map(|x| x.swap_lines()).collect();
-    let a = warpy(&lp, &imgbufsource, &imgbufdestination);
-    let b = warpy(&inv_lp, &imgbufdestination, &imgbufsource);
+    let a = warpy(&lp, &imgbufsource, &imgbufdestination, 0.5);
+    let b = warpy(&inv_lp, &imgbufdestination, &imgbufsource, 0.5);
     a.save("./data/target_source.png").unwrap();
     b.save("./data/source_target.png").unwrap();
     let vec_images = cross_dissolve(&imgbufsource, &a, 0.1);
     let mut i = 0;
-    for _image in vec_images.iter(){
-        _image.save(format!("./data/target_source_{}.png", i)).unwrap();
+    for _image in vec_images.iter() {
+        _image
+            .save(format!("./data/target_source_{}.png", i))
+            .unwrap();
         i += 1;
     }
-
-
 }
-
-
 
 fn main() {
     test_with_simple_polygons()
